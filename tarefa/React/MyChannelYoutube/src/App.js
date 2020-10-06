70dc80aa2ab3fdced002dad7f02ad684c64427e7
Rodrigo <rodrigo@wswork.com.br>
2020-10-06 17:44:34 -0300

    Cria um componente de link.

--- tarefa/React/MyChannelYoutube/src/App.js
+++ tarefa/React/MyChannelYoutube/src/App.js
@@ -9,16 +9,27 @@ function App() {
         <img src={fundo} className="App-fundo" alt="Fundo" />
         <label id="name">Anderson Daniel Langbecker</label>
         <label id="intro">Venha conferir meu canal de gameplays e não esqueça de se inscrever, clicar no sininho e deixar aquele like pra fortalecer. Espero que se divirta!!</label>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/channel/UCyFhAk2wJ-9IWTlfQUwzDFQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NoobZeira
-        </a>
+        
+        <LinkNoobZeira label="NoobZeira 2" link="https://www.youtube.com/channel/UCyFhAk2wJ-9IWTlfQUwzDFQ" />
       </header>
     </div>
+  );
+}
+
+function LinkNoobZeira(props) {
+
+  const meuLabel = props.label;
+  const meuLink = props.link;
+
+  return(
+    <a
+      className="App-link"
+      href={meuLink}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {meuLabel}
+    </a>
   );
 }
 
